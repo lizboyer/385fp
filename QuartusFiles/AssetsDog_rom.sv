38b6dfe795d822268c52cea098e61ad83c9f0a0a
yahavmangel <128866751+yahavmangel@users.noreply.github.com>
2023-04-20 10:58:13 -0500

    4/19 fixes

--- QuartusFiles/AssetsDog_rom.sv
+++ QuartusFiles/AssetsDog_rom.sv
@@ -2,12 +2,10 @@ module AssetsDogs_rom (
 	input logic clock,
 	input logic [4:0] frame,
 	input logic [13:0] address,
-	output logic [3:0] q
-	output logic [5:0] DogSizeX, DogSizeY;
+	output logic [3:0] q,
+	output logic [6:0] DogSizeX, DogSizeY
 );
-
-// logic [3:0] memory [0:9459] /* synthesis ram_init_file = "./AssetsDogs0/AssetsDogs0.mif" */;
-logic [3:0] q0 ,q1, q2, q3, q4, q5; /* synthesis ram_init_file = "./AssetsDogs0/AssetsDogs0.mif" */;
+logic [3:0] q0 ,q1, q2, q3, q4, q5;
 
 
 	AssetsDogs0_rom AssetsDogs0_rom0(.clock, .address(address), .q(q0));
@@ -23,40 +21,50 @@ begin
 	case(frame)
 		5'b00000: begin
 			q = q0;//walk1
-			DogSizeX = 480;
-			DogSizeY = 512;
+			DogSizeX = 7'b1101110;
+			DogSizeY = 7'b1010110;
 		end
 		5'b00001:begin
 			q = q1;//walk2
-			DogSizeX = 480;
-			DogSizeY = 512;
+			DogSizeX = 7'b1101110;
+			DogSizeY = 7'b1010110;
 		end
 		5'b00010:begin
 			q = q2;//walk3
-			DogSizeX = 480;
-			DogSizeY = 512;
+			DogSizeX = 7'b1101110;
+			DogSizeY = 7'b1010110;
 		end
 		5'b00011: begin
 			q = q3;//walk4
-			DogSizeX = 480;
-			DogSizeY = 512;
+			DogSizeX = 7'b1101110;
+			DogSizeY = 7'b1010110;
 		end
 		5'b00100: begin
 			q = q4;//sniff1
-			DogSizeX = 480;
-			DogSizeY = 512;
+			DogSizeX = 7'b1101110;
+			DogSizeY = 7'b1010110;
 		end
 		5'b00101: begin
 			q = q5;//sniff2
-			DogSizeX = 480;
-			DogSizeY = 512;
+			DogSizeX = 7'b1101110;
+			DogSizeY = 7'b1010110;
 		end
 		// 5'b00110: q = q6;//surprised1
 		// 5'b00111: q = q7;//jump1
 		// 5'b01000: q = q8;//jump2
+		default: 
+		begin
+			q = q0;
+			DogSizeX = 7'b0000000;
+			DogSizeY = 7'b0000000;
+		end
+		
 	endcase
 end
-
+//
+//always_ff @ (posedge clock) begin
+//	q <= memory[address];
+//end
 endmodule
 
 module AssetsDogs0_rom (
