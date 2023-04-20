99eafade8856bf90c9ed8473c28fecc13ae11f65
lizboyer <lizboyer01@gmail.com>
2023-04-19 19:44:31 -0500

    added size to rom

--- QuartusFiles/AssetsDog_rom.sv
+++ QuartusFiles/AssetsDog_rom.sv
@@ -10,32 +10,53 @@ module AssetsDogs_rom (
 logic [3:0] q0 ,q1, q2, q3, q4, q5; /* synthesis ram_init_file = "./AssetsDogs0/AssetsDogs0.mif" */;
 
 
-	AssetsDogs0_rom AssetsDogs0_rom0(.clock, .address(address), .q(q0))
-	AssetsDogs1_rom AssetsDogs1_rom0(.clock, .address(address), .q(q1))
-	AssetsDogs2_rom AssetsDogs2_rom0(.clock, .address(address), .q(q2))
-	AssetsDogs3_rom AssetsDogs3_rom0(.clock, .address(address), .q(q3))
-	AssetsDogs4_rom AssetsDogs4_rom0(.clock, .address(address), .q(q4))
-	AssetsDogs5_rom AssetsDogs5_rom0(.clock, .address(address), .q(q5))
+	AssetsDogs0_rom AssetsDogs0_rom0(.clock, .address(address), .q(q0));
+	AssetsDogs1_rom AssetsDogs1_rom0(.clock, .address(address), .q(q1));
+	AssetsDogs2_rom AssetsDogs2_rom0(.clock, .address(address), .q(q2));
+	AssetsDogs3_rom AssetsDogs3_rom0(.clock, .address(address), .q(q3));
+	AssetsDogs4_rom AssetsDogs4_rom0(.clock, .address(address), .q(q4));
+	AssetsDogs5_rom AssetsDogs5_rom0(.clock, .address(address), .q(q5));
 
 
 always_comb
 begin
 	case(frame)
-		5'b00000: q = q0;//walk1
-		5'b00001: q = q1;//walk2
-		5'b00010: q = q2;//walk3
-		5'b00011: q = q3;//walk4
-		5'b00100: q = q4;//sniff1
-		5'b00101: q = q5;//sniff2
+		5'b00000: begin
+			q = q0;//walk1
+			DogSizeX = 480;
+			DogSizeY = 512;
+		end
+		5'b00001:begin
+			q = q1;//walk2
+			DogSizeX = 480;
+			DogSizeY = 512;
+		end
+		5'b00010:begin
+			q = q2;//walk3
+			DogSizeX = 480;
+			DogSizeY = 512;
+		end
+		5'b00011: begin
+			q = q3;//walk4
+			DogSizeX = 480;
+			DogSizeY = 512;
+		end
+		5'b00100: begin
+			q = q4;//sniff1
+			DogSizeX = 480;
+			DogSizeY = 512;
+		end
+		5'b00101: begin
+			q = q5;//sniff2
+			DogSizeX = 480;
+			DogSizeY = 512;
+		end
 		// 5'b00110: q = q6;//surprised1
 		// 5'b00111: q = q7;//jump1
 		// 5'b01000: q = q8;//jump2
 	endcase
 end
 
-always_ff @ (posedge clock) begin
-	q <= memory[address];
-end
 endmodule
 
 module AssetsDogs0_rom (
